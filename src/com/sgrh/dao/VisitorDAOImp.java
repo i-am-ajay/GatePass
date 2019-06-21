7b5a46eefcdc7cca103586e5b3f0da41a2ff9ea5
ajay <ajay.chandel@sgrh.com>
2019-06-21 17:09:14 +0530

    update time code

--- src/com/sgrh/dao/VisitorDAOImp.java
+++ src/com/sgrh/dao/VisitorDAOImp.java
@@ -156,19 +156,15 @@ public class VisitorDAOImp {
 		try {
 			SessionFactory sFactory = factory.getObject();
 			session = sFactory.getCurrentSession();
-			System.out.println("Capture entry time Method called");
+			//System.out.println("Capture entry time Method called");
 			VisitorEntry entry = session.get(VisitorEntry.class, passNo);
 			String entryUserName = Strings.nullToEmpty(entry.getEntryUser());
 			System.out.println("Entry User name " +entryUserName);
 			if(entryUserName.equals("")) {
-				//System.out.println("Flush mode mannual");
-				//session.setHibernateFlushMode(FlushMode.MANUAL);
 				entry.setEntryUser(user);
 				entry.setEntryTime(LocalDateTime.now());
 			}
 			session.update(entry);
-			//session.flush();
-			
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
